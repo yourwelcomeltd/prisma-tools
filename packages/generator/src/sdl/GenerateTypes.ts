eafd384ef1bc22661bb65b99bd202c62dcfa13fe
Andrew Porritt <aporritt@gogo-robot.com>
2024-07-01 16:37:45 +0100

    Fix CreateManyAndReturn type

--- packages/generator/src/sdl/GenerateTypes.ts
+++ packages/generator/src/sdl/GenerateTypes.ts
@@ -43,6 +43,23 @@ export class GenerateTypes {
     );
   }
 
+  getParentType(name: string) {
+    if (['Query', 'Mutation'].includes(name)) {
+      return '{}';
+    }
+    if (name === 'AffectedRowsOutput') {
+      return 'Client.Prisma.BatchPayload';
+    }
+    if (this.isModel(name)) {
+      return name;
+    }
+    if (name.startsWith('CreateMany') && name.endsWith('AndReturnOutputType')) {
+      const innerType = name.replace(/^CreateMany|AndReturnOutputType$/g, '');
+      return `ReturnType<Client.Prisma.${innerType}Delegate["createManyAndReturn"]>`;
+    }
+    return `Client.Prisma.${name}`;
+  }
+
   capital(name: string) {
     return name.charAt(0).toUpperCase() + name.slice(1);
   }
@@ -105,15 +122,7 @@ export class GenerateTypes {
 
       // generate fields
       type.fields.forEach((field) => {
-        const parentType = ['Query', 'Mutation'].includes(type.name)
-          ? '{}'
-          : `Client.${
-              type.name === 'AffectedRowsOutput'
-                ? 'Prisma.BatchPayload'
-                : !this.isModel(type.name)
-                  ? 'Prisma.' + type.name
-                  : type.name
-            }`;
+        const parentType = this.getParentType(type.name);
         const argsType =
           field.args.length > 0
             ? `${['Query', 'Mutation'].includes(type.name) ? '' : type.name}${this.capital(field.name)}Args`
